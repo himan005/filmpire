189ed28a4aa7eef09a1850d1cef89901d5d18aaa
himan005 <himanshugoelj@gmail.com>
2024-02-29 23:33:27 +0530

    Created required components and set routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { CssBaseline } from "@mui/material"
-
+import {Route, Routes} from 'react-router-dom'
+import {Actors, Movies, NavBar, MovieInformation, Profile} from './components/';
 
 const  App = () => (
     <div>
       <CssBaseline />
-      <h1>Filmpire</h1>
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path = "/" exact element={<Movies />} />
+          <Route path = "/profile/:id" element={<Profile />} />
+          <Route path = "/actors/:id" element={<Actors />} />
+          <Route path = "/movie/:id" element={<MovieInformation />} />
+        </Routes>
+      </main>
     </div>
   )
 
