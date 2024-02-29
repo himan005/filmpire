119d4fa0bc10e0606a0835d3742029f04f314ba3
himan005 <himanshugoelj@gmail.com>
2024-02-29 22:35:01 +0530

    Installed needed dependies and used Css Base line for configure base of css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-const  App = () => (
-    <>
-      <div>App</div>
-    </>
-  )
+import React from 'react'
+import { CssBaseline } from "@mui/material"
 
+
+const  App = () => (
+    <div>
+      <CssBaseline />
+      <h1>Filmpire</h1>
+    </div>
+  )
 
 export default App
