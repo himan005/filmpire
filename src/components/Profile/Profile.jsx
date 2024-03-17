0b4562cf42a3ec77df1626c6c5bc3d476ef2f558
himan005 <himanshugoelj@gmail.com>
2024-03-17 14:05:44 +0530

    Added profile page with logout functionality

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { Typography, Button, Box } from '@mui/material'
+import { useSelector } from 'react-redux';
+import { ExitToApp } from '@mui/icons-material';
 
 const Profile = () => {
+  const {user} = useSelector((state) => state.user)
+  const favoriteMovies = []
+  const logout = () =>{
+    localStorage.clear()
+    window.location.href ='/'
+  }
   return (
-    <div>Profile</div>
+    <>
+      <Box display='flex' justifyContent="space-between">
+        <Typography variant='h4' gutterBottom>My Profile</Typography>
+        <Button color="inherit" onClick={logout}>Logout &nbsp; <ExitToApp /></Button>
+      </Box>
+      {!favoriteMovies.length
+      ? <Typography variant='h5'>Add some favorite or playlist movies </Typography>
+      : <Box>Favorite Movies</Box>}
+    </>
   )
 }
 
